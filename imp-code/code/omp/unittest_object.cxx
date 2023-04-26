3e4bfe12cad4019752918255a4b196fddd568ed8
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2023-04-26 09:25:02 -0500

    omp object test up to date

--- imp-code/code/omp/unittest_object.cxx
+++ imp-code/code/omp/unittest_object.cxx
@@ -81,9 +81,9 @@ TEST_CASE( "local domain","[omp][object][02]" ) {
     // setup as in distribution test
     const int points_per_proc = ipower(5,2);
     index_int total_points = points_per_proc*the_env.nprocs();
-    coordinate<index_int,2> omega( total_points );
+    coordinate<index_int,2> omega( endpoint<index_int,2>(total_points) );
     omp_decomposition<2> procs( the_env );
-    omp_distribution omega_p( omega,procs );
+    omp_distribution<2> omega_p( omega,procs );
 
     // create the object
     omp_object xp( omega_p ), yp( omega_p );
@@ -105,8 +105,8 @@ TEST_CASE( "addition","[omp][object][03]" ) {
     index_int total_points = points_per_proc*the_env.nprocs();
     coordinate<index_int,1> omega( total_points );
     omp_decomposition<1> procs( the_env );
-    omp_distribution omega_p( omega,procs );
-    omp_object xp( omega_p ), yp( omega_p );
+    omp_distribution<1> omega_p( omega,procs );
+    omp_object<1> xp( omega_p ), yp( omega_p );
 
     // set and operate
     REQUIRE_NOTHROW( xp.set_constant(2.5) );
@@ -123,8 +123,8 @@ TEST_CASE( "addition","[omp][object][03]" ) {
     index_int total_points = points_per_proc*the_env.nprocs();
     coordinate<index_int,2> omega( total_points );
     omp_decomposition<2> procs( the_env );
-    omp_distribution omega_p( omega,procs );
-    omp_object xp( omega_p ), yp( omega_p );
+    omp_distribution<2> omega_p( omega,procs );
+    omp_object<2> xp( omega_p ), yp( omega_p );
 
     // set and operate
     REQUIRE_NOTHROW( xp.set_constant(2.5) );
@@ -136,7 +136,7 @@ TEST_CASE( "addition","[omp][object][03]" ) {
   }
 }
 
-TEST_CASE( "norm","[omp][object][04]" ) {
+TEST_CASE( "norm","[object][04]" ) {
   {
     INFO("1D");
     // big object
@@ -144,19 +144,68 @@ TEST_CASE( "norm","[omp][object][04]" ) {
     index_int total_points = points_per_proc*the_env.nprocs();
     coordinate<index_int,1> omega( total_points );
     omp_decomposition<1> procs( the_env );
-    omp_distribution omega_p( omega,procs );
+    omp_distribution<1> omega_p( omega,procs );
+    omp_object xp( omega_p ), yp( omega_p );
+    REQUIRE_NOTHROW( xp.set_constant(1.) );
+
+    //scalar
+    REQUIRE_NOTHROW( omp_object( replicated_scalar_distribution( procs ) ) );
+    auto norm_value = omp_object( replicated_scalar_distribution( procs ) );
+
+    
+    REQUIRE_THROWS( norm(xp,xp,the_env) );
+    REQUIRE_NOTHROW( norm(norm_value,xp,the_env) );
+    double *norm_data;
+    REQUIRE_NOTHROW( norm_data = norm_value.data() );
+    REQUIRE( norm_data[0]==static_cast<double>(total_points) );
+  }
+  {
+    INFO("2D");
+    // big object
+    const int points_per_proc = ipower(5,2);
+    index_int total_points = points_per_proc*the_env.nprocs();
+    coordinate<index_int,2> omega( endpoint<index_int,2>(total_points) );
+    omp_decomposition<2> procs( the_env );
+    omp_distribution<2> omega_p( omega,procs );
     omp_object xp( omega_p ), yp( omega_p );
     xp.set_constant(1.);
 
     //scalar
-    REQUIRE_NOTHROW( omp_object( replicated_scalar_distribution<1>( procs ) ) );
-    auto norm_value = omp_object( replicated_scalar_distribution<1>( procs ) );
+    REQUIRE_NOTHROW( omp_object( replicated_scalar_distribution( procs ) ) );
+    auto norm_value = omp_object( replicated_scalar_distribution( procs ) );
 
     
-    REQUIRE_THROWS( compute_norm(xp,xp,the_env) );
-    REQUIRE_NOTHROW( compute_norm(norm_value,xp,the_env) );
+    REQUIRE_THROWS( norm(xp,xp,the_env) );
+    REQUIRE_NOTHROW( norm(norm_value,xp,the_env) );
     double *norm_data;
     REQUIRE_NOTHROW( norm_data = norm_value.data() );
     REQUIRE( norm_data[0]==static_cast<double>(total_points) );
+  }
+}
+
+TEST_CASE( "inner product","[omp][object][05]" ) {
+  {
+    INFO("1D");
+    // big object
+    const int points_per_proc = ipower(5,1);
+    index_int total_points = points_per_proc*the_env.nprocs();
+    coordinate<index_int,1> omega( total_points );
+    omp_decomposition<1> procs( the_env );
+    omp_distribution<1> omega_p( omega,procs );
+    omp_object xp( omega_p ), yp( omega_p );
+    REQUIRE_NOTHROW( xp.set_constant(1.) );
+    REQUIRE_NOTHROW( yp.set_constant(2.) );
+
+    //scalar
+    REQUIRE_NOTHROW( omp_object( replicated_scalar_distribution( procs ) ) );
+    auto norm_value = omp_object( replicated_scalar_distribution( procs ) );
+
+    
+    REQUIRE_THROWS( inner_product(xp,xp,yp,the_env) );
+    REQUIRE_THROWS( inner_product(norm_value,xp,norm_value,the_env) );
+    REQUIRE_NOTHROW( inner_product(norm_value,xp,yp,the_env) );
+    double *norm_data;
+    REQUIRE_NOTHROW( norm_data = norm_value.data() );
+    REQUIRE( norm_data[0]==2.*static_cast<double>(total_points) );
   }
 }
