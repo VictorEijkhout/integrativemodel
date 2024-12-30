94ebeb763dc786f9ba547732250ca68ecc795377
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2024-12-30 10:57:31 -0600

    trivial file; should probably go

---
+++ imp-code/code/seq/seq_static_vars.h
@@ -0,0 +1,23 @@
+/****************************************************************
+ ****
+ **** This file is part of the prototype implementation of
+ **** the Integrative Model for Parallelism
+ ****
+ **** copyright Victor Eijkhout 2014-2024
+ ****
+ **** Statically defined variables for a SEQ-based run
+ ****
+ ****************************************************************/
+
+#ifndef SEQ_STATIC_VARS_H
+#define SEQ_STATIC_VARS_H
+
+#ifndef EXTERN
+#ifdef seq_STATIC_VARS_HERE
+#define EXTERN
+#else
+#define EXTERN extern
+#endif
+#endif
+
+#endif
