36a21e155e0a580c6b8c109a9f3cf4f38f02d6fc
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2025-01-10 12:01:22 -0600

    coordinate dimensionality

--- imp-code/code/omp/unittest_coord.cxx
+++ imp-code/code/omp/unittest_coord.cxx
@@ -3,7 +3,7 @@
  **** This file is part of the prototype implementation of
  **** the Integrative Model for Parallelism
  ****
- **** copyright Victor Eijkhout 2014-2022
+ **** copyright Victor Eijkhout 2014-2025
  ****
  **** Unit tests for the OMP product backend of IMP
  **** based on the CATCH framework (https://github.com/philsquared/Catch)
@@ -28,20 +28,20 @@ TEST_CASE( "end points","[arch][01]" ) {
   {
     INFO( "p3 d1" );
     auto p3d1 = endpoint<int,1>(3);
-    REQUIRE( p3d1.size()==1 );
+    REQUIRE( p3d1.dimensionality()==1 );
     REQUIRE( p3d1.at(0)==3 );
   }
   {
     INFO( "p4 d2" );
     auto p4d2 = endpoint<int,2>(4);
-    REQUIRE( p4d2.size()==2 );
+    REQUIRE( p4d2.dimensionality()==2 );
     REQUIRE( p4d2.at(0)==2 );
     REQUIRE( p4d2.at(1)==2 );
   }
   {
     INFO( "p30 d3" );
     auto p30d3 = endpoint<int,3>(30);
-    REQUIRE( p30d3.size()==3 );
+    REQUIRE( p30d3.dimensionality()==3 );
     REQUIRE( p30d3.at(0)==5 );
     REQUIRE( p30d3.at(1)==3 );
     REQUIRE( p30d3.at(2)==2 );
